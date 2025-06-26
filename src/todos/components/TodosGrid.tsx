077b62292004c8d84b58ff0572cc4e141736ca16
Carlos Vasco <vasco.1019@gmail.com>
2025-06-25 19:08:35 -0500

    feat: add todo API endpoints and update UI components

--- src/todos/components/TodosGrid.tsx
+++ src/todos/components/TodosGrid.tsx
@@ -2,29 +2,24 @@
 
 import { Todo } from "@/generated/prisma/client"
 import { TodoItem } from "./TodoItem";
-
 import * as todosApi from '../helpers/todos';
 import { useRouter } from "next/navigation";
-
 
 interface Props {
   todos?: Todo[];
 }
 
-
 export const TodosGrid = ({ todos = [] }: Props) => {
 
   const router = useRouter();
 
-  
-  const toggleTodo = async(id: string, complete: boolean) => {
-    const updatedTodo = await todosApi.updateTodo( id, complete );
-    console.log({updatedTodo});
+  const toggleTodo = async (id: string, complete: boolean) => {
+    const updatedTodo = await todosApi.updateTodo(id, complete);
+    console.log({ id, updatedTodo });
     router.refresh();
-  }
+  };
 
-
-  
+ 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
       {
